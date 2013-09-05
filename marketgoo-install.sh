282ebf52a1a3e5a04fffe6bb8ea9f270ed3ddefc
Jose Miguel Pérez Ruiz <josemiguel@perezruiz.com>
2013-09-05 03:35:46 +0200

    Pruebas

--- marketgoo-install.sh
+++ marketgoo-install.sh
@@ -10,22 +10,69 @@
 #   MarketGoo Plug-in for cPanel      |___/             
 #
 #
-#   Execute this file in your shell:
+#   Download and execute this file in your shell:
+#   $ wget https://raw.github.com/twoixter/nimiedades/master/marketgoo-install.sh
 #   $ /bin/sh ./marketgoo-install.sh
+#
+#   Execute installer directly from GitHub
+#   $ wget -O - http://dl.dropbox.com/u/11210438/flockonus-stack.sh | sh
 #
 ##############################################################################
 
 WHITE=$(tput setaf 7 ; tput bold)
-NO_COLOUR=$(tput sgr0)
+RED=$(tput setaf 1 ; tput bold)
+RESET=$(tput sgr0)
 WHMROOT=/usr/local/cpanel/whostmgr
+TMPNAME=$(mktemp -d marketgooplugin.XXXXXXXXX)
+CPVERSION=$(cat 2>/dev/null /usr/local/cpanel/version)
+
+cleanup()
+{
+    local rc=$?
+    trap - EXIT
+
+    rm -rf $TMPNAME
+    exit $rc
+}
+
+download_latest()
+{
+    echo
+    echo "${WHITE}Downloading latest plug-in version${RESET}"
+    mkdir -p $TMPNAME
+    wget -q -O $TMPNAME/mktsrc.tar.gz --no-check-certificate https://github.com/twoixter/nimiedades/archive/master.tar.gz > /dev/null
+}
 
 install_whm_addon()
 {
-    echo ${WHITE}Installing WHM AddOn${NO_COLOUR}
+    echo "${WHITE}Installing WHM AddOn${NO_COLOUR}"
 
-    mkdir -p /var/cpanel/apps
-    
+#    mkdir -p /var/cpanel/apps
 }
 
+trap cleanup HUP PIPE INT QUIT TERM EXIT
 
-install
+if [ ! -f /usr/local/cpanel/version ]; then
+    echo
+    echo "${RED}***** Cpanel not found *****${RESET}"
+    echo "${WHITE}Are you sure you're running this on a Cpanel server?${RESET}"
+    echo
+    exit
+fi
+
+if [ ! -f /usr/local/cpanel/Cpanel/LiveAPI.pm ]; then
+    echo
+    echo "${RED}***** Cpanel::LiveAPI not found. *****${RESET}"
+    echo "${WHITE}MarketGoo Plugin requires Cpanel::LiveAPI.${RESET}"
+    echo "Please ensure you are running at least Cpanel version 11.32.2."
+    echo "(current Cpanel version: $CPVERSION)"
+    echo
+    exit
+fi
+
+[ -d $TMPNAME ] || download_latest
+
+install_whm_addon
+
+echo "${WHITE}*** DONE ***${RESET}"
+rm -rf $TMPNAME
