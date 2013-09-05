9d75bc99dfe694b2813434ebafb75f894010767d
Jose Miguel Pérez Ruiz <josemiguel@perezruiz.com>
2013-09-05 05:09:22 +0200

    pruebas

--- marketgoo-install.sh
+++ marketgoo-install.sh
@@ -49,7 +49,8 @@ install_whm_addon()
 
     cp -r $SRCDIR/whm/marketgoo/ $WHMROOT/docroot/ >/dev/null 2>&1
     cp -f $SRCDIR/whm/addon_marketgoo.cgi $WHMROOT/docroot/cgi/ >/dev/null 2>&1
-    cp -f $SRCDIR/whm/marketgoo.gif $WHMROOT/docroot/addon_plugins/ >/dev/null 2>&1
+    cp -f $SRCDIR/whm/marketgoo.gif $WHMROOT/docroot/themes/x/icons/ >/dev/null 2>&1
+#    chmod 0755 $WHMROOT/docroot/cgi/addon_sitelock.cgi
 }
 
 install_cpanel_plugin()
