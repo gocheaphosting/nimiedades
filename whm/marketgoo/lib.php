fea086ff971da48c97184f67565089ed8fc44f3a
Jose Miguel Pérez Ruiz <josemiguel@perezruiz.com>
2013-10-01 04:37:56 +0200

    Pruebas de partnerid

--- whm/marketgoo/lib.php
+++ whm/marketgoo/lib.php
@@ -13,6 +13,7 @@
  */
 
 define("MKTGOO_PARTNERID_FILE", ".marketgoo_partner_id");
+define("MKTGOO_REMOTE_REPOSITORY", "github.com/twoixter/nimiedades");
 
 //-----------------------------------------------------------------------------
 function generate_partnerid()
